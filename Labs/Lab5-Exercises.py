f285d8356a0407b0f66a3a7e870e7eec7e1ba639
E Nemeti <enemeti@emory.edu>
2024-09-30 19:46:37 -0400

    minor fixes & made lab 5

--- Labs/Lab5-Exercises.py
+++ Labs/Lab5-Exercises.py
@@ -9,44 +9,26 @@ Initial:
 # [10 pts] Exercise 1: Booleans # 
 
 # [2 pts] Create three variables x, y, and z. Assign them as 5, 10, and 0 respectively. 
-x = 5
-y = 10
-z = 0
 
 # Compare the variables using comparison operators <, !=, ==, >=.
 # For each comparison, print the boolean result. 
 
 # [2 pts] Check if x is less than y.
-print("x < y:", x < y) 
 
 # [2 pts] Check if x is not equal to y. 
-print("x != y:", x != y) 
 
 # [2 pts] Check if z is equal to 0. 
-print("z == 0:", z == 0) 
 
 # [2 pts] Check if x is greater than or equal to y. 
-print("x >= y:", x >= y) 
 
 
 # [10 pts] Exercise 2: Decision Programs # 
 
 # [1 pts] Prompt the user to enter an integer.
-number = int(input("Enter a number: "))
 
 # [4 pts] Use if/else to check whether the integer is a positive or negative number, and print the result with a message.
-if number > 0:
-    print("The number is positive")
-else:
-    print("The number is negative or zero")
     
 # [1 pts] Prompt the user to enter a new integer.
-number2 = int(input("Enter a number: "))
 
 # [4 pts] Use if/else to now check whether the integer is even or odd, and print the result with a message.
 # Hint: use the remainder to verify if a number is odd/even.
-
-if number2 % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
--- Labs/tests.py
+++ Labs/tests.py
@@ -1,3 +1,9 @@
-text = "  Hello world this is Python  "
-words = text.split()  # Splits at any whitespace and removes extra spaces
-print(words)
+number2 = int(input("Enter a number: "))
+
+# [4 pts] Use if/else to now check whether the integer is even or odd, and print the result with a message.
+# Hint: use the remainder to verify if a number is odd/even.
+
+if number2 % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
