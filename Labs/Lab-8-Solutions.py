5865d690e8c4b41721acce85d820899a43349778
E Nemeti <enemeti@emory.edu>
2024-11-08 10:24:30 -0500

    hw 5 and lab 8 upload

---
+++ Labs/Lab-8-Solutions.py
@@ -0,0 +1,32 @@
+# Task 1 - Multiplication Table (5 pts)
+
+# (1 pt) Defining the function named multiplication_table with a parameter number
+def multiplication_table(number):
+    # (2 pts) Using a for loop to print the multiplication table from 1 to 10
+    for i in range(1, 11):
+        print(f"{number} x {i} = {number * i}")
+
+# (1 pt) Asking the user for input
+user_number = int(input("Enter a number: "))
+# (1 pt) Calling the function with the user’s input
+multiplication_table(user_number)
+
+# Task 2 - Even or Odd Game (15 pts)
+
+# (3 pts) Defining the function is_even to take a parameter number
+def is_even(number):
+    # (2 pts) Returning True if number is even, False if odd
+    return number % 2 == 0
+
+# (3 pts) Defining the function play_even_odd_game
+def play_even_odd_game():
+    # (3 pts) Asking the user to enter a number between 1 and 50
+    user_number = int(input("Enter a number between 1 and 50: "))
+    # (2 pts) Using is_even to check if the number is even or odd and print the result
+    if is_even(user_number):
+        print("The number is even!")
+    else:
+        print("The number is odd!")
+
+# (2 pts) Calling play_even_odd_game to start the game
+play_even_odd_game()
--- Labs/tests.py
+++ Labs/tests.py
@@ -1,34 +1,27 @@
-# Question 6: .format() Method with Single-Line Strings (Part 1)
-# [7 pts] (Code) 
-# Use the .format() method to print the message: 
-# "My name is <your name> and I am <your age> years old."
 
-# [3 pts] (Written) Name up to 3 reasons why using .format() method makes the code more readable compared to concatenating strings with +?
+"""
+[20 pts] Question 5
 
-your_name="Kevin"
-your_age="18"
-print("My name is {} and I am {} years old.".format(your_name,your_age))
-print("By using the format function, I'm able to put make my long sentences much shorter, avoiding the usage of back slashes like I have previously used\
-      It's also better because I don't have to enter the same name or number again and can just put in a variable instead.\
-      If I want to change a the name that appears multiple times then I can do that once and all of the names in the string will change, making it much easier than having to change each name. ")
+Write a program that takes a user's grade as input and categorizes it as follows:
+- If the grade is 90 or higher, print "A".
+- If the grade is between 80 and 89, print "B".
+- If the grade is between 70 and 79, print "C".
+- If the grade is between 60 and 69, print "D".
+- If the grade is below 60, print "F".
+Hint: make sure to use each at least once: if, elif, and else.
+Hint: make sure the number entered is within a valid range.
+"""
+print("Question 5")
+# Write code below. Paste the input/output of each condition as a comment below. Make sure it runs without error. #
 
-
-# Question 7: .format() Method with Multi-Line Strings (Part 2)
-
-# [15 pts] (Code) 
-# Write a Python program that asks user input for two questions:
-# Why they are taking the course?
-# What interests them most about Python? 
-# Use a multi-line string with triple quotes to display their answers in the following format:
-
-# My Course Reflection
-# I'm taking this course because: <user answer>
-# What excites me most about learning Python is: <user answer>
-
-# [5 pts] (Written) What benefits can you observe from using triple quotes instead of adding \n at every line?
-x=input("Why are you taking this course:")
-y=input("What interests you most about python:")
-print(f"""My Course reflection
-I'm taking this course because {x}
-What excites me most about learning Python is {y}""")
-print("It's more convenient because having to add commas and \n everytime is tedious and prone to error")
+g = int(input("Please enter your integer grade: "))
+if g>=90:
+    print("A")
+elif g>=80 and g<=89:
+    print("B")
+elif g>=70 and g<=79:
+    print("C")
+elif g>=60 and g<=69:
+    print("D")
+else:
+    print("F")
